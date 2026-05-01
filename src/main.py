ddc3bfd3db31671efefe97930578e9a8b9c4ad41
Dinorah Farias <dinorahfarias@outlook.com>
2026-05-01 11:22:27 -0300

    feat: update main to use cli entrypoint #3

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Hello Toy-Bank!!!")
+
+from accounts import AccountStore
+from cli import run_cli
+
+
+def main():
+	store = AccountStore()
+	run_cli(store)
+
+
+if __name__ == "__main__":
+	main()
